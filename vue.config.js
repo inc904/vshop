6573b686786bd3fa8bfd583b53cbad6ccb360a1c
inc904 <jay1845@aliyun.com>
2021-03-17 14:07:35 +0800

    proxy target changed to domain

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,15 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://192.168.221.132',
+        // target: 'http://192.168.221.132',
+        target: 'shop.admin.com',
         ws: true,
         changeOrigin: true,
         // logLevel: 'debug',
         pathRewrite: {
-          '/api': '/',
-        },
-      },
-    },
-  },
+          '/api': '/'
+        }
+      }
+    }
+  }
 }
